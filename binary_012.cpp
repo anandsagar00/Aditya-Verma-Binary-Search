cafc9fea938b3b4e89feea06cf357f1e5420e7fc
anandsagar00 <80447047+anandsagar00@users.noreply.github.com>
2022-06-26 02:09:57 +0530

    index of first 1 in a "Infinite" Binary Sorted Array

---
+++ binary_012.cpp
@@ -0,0 +1,13 @@
+//Find the index of first 1 in a "Infinite" Binary Sorted Array
+
+// This is a combination of first occurence + search in an infinite array
+
+// (Use your common sense - No need of coding in this )
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //code yourself
+}
